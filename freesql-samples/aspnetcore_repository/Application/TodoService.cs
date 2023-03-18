10228b5ce40fab416744b8baae3034d36e423fa9
igeekfan <luoyunchong@foxmail.com>
2023-03-18 19:24:56 +0800

    Add TodoService

---
+++ freesql-samples/aspnetcore_repository/Application/TodoService.cs
@@ -0,0 +1,32 @@
+﻿using aspnetcore_repository.DTO;
+using aspnetcore_repository.Models;
+using IGeekFan.FreeKit.Extras.FreeSql;
+
+namespace aspnetcore_repository.Application;
+
+public class TodoService : ITodoService
+{
+    private readonly IAuditBaseRepository<Todo, int> _repository;
+
+    public TodoService(IAuditBaseRepository<Todo, int> repository)
+    {
+        _repository = repository;
+    }
+
+
+    [Transactional]
+    public async Task Update(UpdateTodo updateTodo)
+    {
+        Todo todo = await _repository.GetAsync(updateTodo.Id);
+        todo.Message = updateTodo.Message;
+        todo.IsDone = updateTodo.IsDone;
+        if (todo.IsDone) todo.DoneTime = DateTime.Now;
+        todo.NotifictionTime = updateTodo.NotifictionTime;
+        await _repository.UpdateAsync(todo);
+    }
+}
+
+public interface ITodoService
+{
+    Task Update(UpdateTodo updateTodo);
+}
--- freesql-samples/aspnetcore_repository/Controllers/TodoController.cs
+++ freesql-samples/aspnetcore_repository/Controllers/TodoController.cs
@@ -1,106 +1,113 @@
+using aspnetcore_repository.Application;
 using aspnetcore_repository.DTO;
 using aspnetcore_repository.Models;
-using FreeSql.Internal.Model;
 using IGeekFan.FreeKit.Extras.Extensions;
 using IGeekFan.FreeKit.Extras.FreeSql;
 using Microsoft.AspNetCore.Mvc;
 
-namespace aspnetcore_repository.Controllers
+namespace aspnetcore_repository.Controllers;
+
+/// <summary>
+/// 2.基于审计仓储类（IAuditBaseRepository）的实现 
+/// </summary>
+[ApiController]
+//[Authorize]
+[Route("[controller]")]
+public class TodoController : ControllerBase
 {
-    /// <summary>
-    /// 2.基于审计仓储类（IAuditBaseRepository）的实现 
-    /// </summary>
-    [ApiController]
-    //[Authorize]
-    [Route("[controller]")]
-    public class TodoController : ControllerBase
+    private readonly IAuditBaseRepository<Todo, int> _repository;
+    private readonly ILogger<TodoController> _logger;
+    private readonly ITodoService _todoService;
+
+    public TodoController(ILogger<TodoController> logger, IAuditBaseRepository<Todo, int> repository, ITodoService todoService)
     {
-        private readonly IAuditBaseRepository<Todo, int> _repository;
-        private readonly ILogger<TodoController> _logger;
+        _logger = logger;
+        _repository = repository;
+        _todoService = todoService;
+    }
 
-        public TodoController(ILogger<TodoController> logger, IAuditBaseRepository<Todo, int> repository)
+    /// <summary>
+    /// 分页获取Todo
+    /// </summary>
+    /// <param name="pagingInfo"></param>
+    /// <returns></returns>
+    [HttpGet]
+    public async Task<IResult> GetPageAsync([FromQuery] QueryTodo pagingInfo)
+    {
+        List<Todo> data = await _repository.Select
+            .WhereIf(pagingInfo.Message.IsNotNullOrWhiteSpace(), r => r.Message.Contains(pagingInfo.Message))
+            .Page(pagingInfo)
+            .ToListAsync();
+
+        return Results.Ok(new { data = data, count = pagingInfo.Count });
+    }
+
+
+    /// <summary>
+    /// 获取一个TODO
+    /// </summary>
+    /// <param name="id"></param>
+    /// <returns></returns>
+    [HttpGet("{id}")]
+    public async Task<IResult> GetAsync(int id)
+    {
+        Todo todo = await _repository.GetAsync(id);
+        _logger.LogInformation($"Todo get success");
+        return Results.Ok(todo);
+    }
+
+    /// <summary>
+    /// 创建一个TODO
+    /// </summary>
+    /// <param name="createTodo"></param>
+    /// <returns></returns>
+    [HttpPost]
+    public async Task<IResult> Create([FromBody] CreateTodo createTodo)
+    {
+        Todo todo = new Todo { IsDone = createTodo.IsDone, Message = createTodo.Message, NotifictionTime = createTodo.NotifictionTime };
+        await _repository.InsertAsync(todo);
+        _logger.LogInformation($"todo crate success");
+        return Results.Ok(todo);
+    }
+
+    /// <summary>
+    /// 更新一个TODO
+    /// </summary>
+    /// <param name="updateTodo"></param>
+    /// <returns></returns>
+    [HttpPut]
+    public async Task<IResult> Update([FromBody] UpdateTodo updateTodo)
+    {
+        Todo todo = await _repository.GetAsync(updateTodo.Id);
+        if (todo == null)
         {
-            _logger = logger;
-            _repository = repository;
+            return Results.BadRequest($"to do id {updateTodo.Id} not found.");
         }
-
-        /// <summary>
-        /// 分页获取Todo
-        /// </summary>
-        /// <param name="pagingInfo"></param>
-        /// <returns></returns>
-        [HttpGet]
-        public async Task<IResult> GetPageAsync([FromQuery] QueryTodo pagingInfo)
-        {
-            List<Todo> data = await _repository.Select
-                .WhereIf(pagingInfo.Message.IsNotNullOrWhiteSpace(), r => r.Message.Contains(pagingInfo.Message))
-                .Page(pagingInfo)
-                .ToListAsync();
-
-            return Results.Ok(new { data = data, count = pagingInfo.Count });
-        }
-
-
-        /// <summary>
-        /// 获取一个TODO
-        /// </summary>
-        /// <param name="id"></param>
-        /// <returns></returns>
-        [HttpGet("{id}")]
-        public async Task<IResult> GetAsync(int id)
-        {
-            Todo todo = await _repository.GetAsync(id);
-            _logger.LogInformation($"Todo get success");
-            return Results.Ok(todo);
-        }
-
-        /// <summary>
-        /// 创建一个TODO
-        /// </summary>
-        /// <param name="createTodo"></param>
-        /// <returns></returns>
-        [HttpPost]
-        public async Task<IResult> Create([FromBody] CreateTodo createTodo)
-        {
-            Todo todo = new Todo { IsDone = createTodo.IsDone, Message = createTodo.Message, NotifictionTime = createTodo.NotifictionTime };
-            await _repository.InsertAsync(todo);
-            _logger.LogInformation($"todo crate success");
-            return Results.Ok(todo);
-        }
-
-        /// <summary>
-        /// 更新一个TODO
-        /// </summary>
-        /// <param name="updateTodo"></param>
-        /// <returns></returns>
-        [HttpPut]
-        public async Task<IResult> Update([FromBody] UpdateTodo updateTodo)
-        {
-            Todo todo = await _repository.GetAsync(updateTodo.Id);
-            if (todo == null)
-            {
-                return Results.BadRequest($"to do id {updateTodo.Id} not found.");
-            }
-            todo.Message = updateTodo.Message;
-            todo.IsDone = updateTodo.IsDone;
-            if (todo.IsDone) todo.DoneTime = DateTime.Now;
-            todo.NotifictionTime = updateTodo.NotifictionTime;
-            await _repository.UpdateAsync(todo);
-            _logger.LogInformation($"todo update success");
-            return Results.Ok(todo);
-        }
-
-        /// <summary>
-        /// 删除一个TODO 
-        /// </summary>
-        /// <param name="id"></param>
-        /// <returns></returns>
-        [HttpDelete]
-        public async Task<IResult> Delete(int id)
-        {
-            int row = await _repository.DeleteAsync(id);
-            _logger.LogInformation($"todo delete success");
-            return Results.Ok(row);
-        }
+        todo.Message = updateTodo.Message;
+        todo.IsDone = updateTodo.IsDone;
+        if (todo.IsDone) todo.DoneTime = DateTime.Now;
+        todo.NotifictionTime = updateTodo.NotifictionTime;
+        await _repository.UpdateAsync(todo);
+        _logger.LogInformation($"todo update success");
+        return Results.Ok(todo);
+    }
+    [HttpPut("update")]
+    public async Task<IResult> UpdateAsync([FromBody] UpdateTodo updateTodo)
+    {
+        await _todoService.Update(updateTodo);
+        _logger.LogInformation($"todo update success");
+        return Results.Ok(updateTodo);
+    }
+    /// <summary>
+    /// 删除一个TODO 
+    /// </summary>
+    /// <param name="id"></param>
+    /// <returns></returns>
+    [HttpDelete]
+    public async Task<IResult> Delete(int id)
+    {
+        int row = await _repository.DeleteAsync(id);
+        _logger.LogInformation($"todo delete success");
+        return Results.Ok(row);
     }
 }
